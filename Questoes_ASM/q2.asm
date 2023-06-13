bd42495cecc28ecf286cbb42e7284e85df57a758
Alex Carvalho <jac@cin.ufpe.br>
2023-06-13 16:26:24 -0300

    Update q2.asm

--- Questoes_ASM/q2.asm
+++ Questoes_ASM/q2.asm
@@ -1,16 +1,18 @@
-;A QUESTÃO AINDA NÃO ESTÁ FINALIZADA
-
 org 0x7c00
 jmp 0x0000:start
 
-string: times 9 db 0, 0
+string: times 9 db 0
 start:
     mov di, string
     call gets
     call getchar
-    ;call printTam
+    mov bl, al
+    call putchar
+    call endl
+    call printTam
+    jmp $
 
-putchar:
+putchar:    
     mov ah, 0x0e
     int 10h
     ret
@@ -18,6 +20,13 @@ putchar:
 getchar:
     mov ah, 0x00
     int 16h
+    ret
+
+endl:
+    mov al, 0x0a
+    call putchar
+    mov al, 0x0d
+    call putchar
     ret
 
 delchar:
@@ -33,27 +42,30 @@ delchar:
     ret
 
 gets:
-    .loop1:
+    xor cx, cx
+    mov di, cx
+
+    .loop:
         call getchar
         cmp al, 0x08
         je .backspace
         cmp al, 0x0d
         je .done
         cmp cl, 50
-        je .loop1
+        je .loop
         stosb
         inc cl
         call putchar
-        jmp .loop1
+        jmp .loop
         
         .backspace:
             cmp cl, 0
-            je .loop1
+            je .loop
             dec di
             dec cl
             mov byte[di], 0
             call delchar
-            jmp .loop1
+            jmp .loop
 
         .done:
             mov al, 0
@@ -62,23 +74,47 @@ gets:
             call putchar
             mov al, 0x0d
             call putchar
+
             ret 
 
+printOcorr:
+
+
 printTam:
+    xor dl, dl
+    xor cl, cl
+
     .loop:
         lodsb
+
         cmp al, 0
         je .endloop
 
-        call putchar
+        cmp al, bl
+        je .incr
+        
+        inc dl
 
         jmp .loop
 
-    call putchar
+    .incr:
+        inc cl
+        inc dl 
+        jmp .loop
 
     .endloop:
-        ret
+        
+        mov al, cl
+        add al, 48
+        call putchar
 
+        mov al, 47
+        call putchar
+
+        mov al, dl
+        add al, 48
+        call putchar
+        ret
 
 times 510 - ($ - $$) db 0
 dw 0xaa55
