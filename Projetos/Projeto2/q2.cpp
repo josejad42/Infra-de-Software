7db4f8d3706c6e1b5414c907a900f5d731d5bf8a
Alex Carvalho <jac@cin.ufpe.br>
2023-08-26 22:03:01 -0300

    Código 50% pronto

--- Projetos/Projeto2/q2.cpp
+++ Projetos/Projeto2/q2.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<pthread.h>
 #define NUM_THREADS 5
+#define TAM_GRAPH 6
 
 using namespace std;
 
@@ -8,7 +9,6 @@ class Graph{
     public:
     int **matrix;
     int numEdge;
-    int *mark;
     int n;
 
     Graph(int nv){
@@ -20,96 +20,65 @@ class Graph{
                 matrix[i][j] = 0;
             }
         }
-
-        mark = new int[n];
-        for(int i = 0; i<n; i++){
-            mark[i] = 0;
-        }
         numEdge = 0;
     }
 
-    ~Graph() {
-        for (int i = 0; i<n; i++) {
-            delete[] matrix[i];
-        }
-        delete[] matrix;
-        delete[] mark;
-    }
-
-    int first(int v){
-        for(int i = 0; i<n; i++){
-            if(matrix[v][i]) return i;
-        }
-        return n;
-    }
-
-    int next(int v, int w){
-        for(int i = w+1; i<n; i++){
-            if(matrix[v][i]) return i;
-        }
-        return n;
-    }
-
-    void setEdge(int i, int j, int wt){
+    void setEdge(int i, int j){
         if(matrix[i][j] == 0) numEdge++;
-        matrix[i][j] = wt;
+        matrix[i][j] = 1;
     } 
-
-    void delEdge(int i, int j){
-        if(matrix[i][j]) numEdge--;
-        matrix[i][j] = 0;
-    }
-
-    void setMark(int i, int val){
-        mark[i] = val;
-    }
-
-    int getMark(int i){
-        return mark[i];
-    }
-
+    
 };
 
-Graph grafo(10);
+Graph grafo(TAM_GRAPH);
 
-void *DFS(void* threadid){
-    int v = *((int*) threadid);
-    printf("Vértice %d\n", v);
+bool DFS(int tid, int v, bool *mark){
+    printf("Thread %d Vértice %d\n", tid, v);
     
-    grafo.setMark(v, 1);
+    mark[v] = true;
     
-    int *w;
-    w = (int*) malloc(sizeof(int));
-    *w = grafo.first(v);
-    
-    while(*w<grafo.n){
-        if(grafo.getMark(*w) == 0) DFS((void*)w);
-        *w = grafo.next(v, *w);
+    for (int i = 0; i<grafo.n; i++){
+        if((grafo.matrix[v][i] == 1) && (mark[i] == false)) return DFS(tid,i,mark);
+        if((grafo.matrix[v][i] == 1) && (mark[i] == true)) return true;
     }
-    pthread_exit(NULL);
+    return false;
 }
 
+void *FindDeadlock(void* threadid){
+    int tid = *((int*) threadid);
+    printf("Thread %d Vértice %d\n", tid, tid);
+    
+    int size = grafo.n;
+    bool mark[size];
+    
+    mark[tid] = true;
+    
+    for(int i = tid; i<size; i++){
+        bool deadlock;
+        if(mark[i] == false) deadlock = DFS(tid,i,mark);
+        if (deadlock == true){
+            printf("Deadlock encontrado.\n");
+            exit(-1);
+        }
+    }
+}
 
 int main(){
-    grafo.setEdge(0, 4, 1);
-    grafo.setEdge(0, 1, 1);
-    grafo.setEdge(1, 3, 1); 
-    grafo.setEdge(2, 4, 1);
-    grafo.setEdge(3, 2, 1);
-    grafo.setEdge(4, 1, 1);
-    grafo.setEdge(4, 9, 1);
-    grafo.setEdge(3, 5, 1);
-    grafo.setEdge(5, 8, 1);
-    grafo.setEdge(8, 7, 1);
-    grafo.setEdge(9, 8, 0);
+    grafo.setEdge(0, 3);
+    grafo.setEdge(1, 5);
+    grafo.setEdge(1, 2);
+    grafo.setEdge(2, 3);
+    grafo.setEdge(4, 1);
+    grafo.setEdge(4, 5);
+    grafo.setEdge(5, 3);
     
     pthread_t threads[NUM_THREADS];
-    int *taskids[NUM_THREADS];
+    int *threadids[NUM_THREADS];
     int rc;
     for(int i=0;i<NUM_THREADS;i++){
-        taskids[i] = (int*) malloc(sizeof(int));
-        *taskids[i] = i;
-        rc = pthread_create(&threads[i],NULL,DFS,(void*)taskids[i]);
+        threadids[i] = (int*) malloc(sizeof(int));
+        *threadids[i] = i;
+        rc = pthread_create(&threads[i],NULL,FindDeadlock,(void*)threadids[i]);
         if(rc){
             printf("Erro! Código de retorno %d", rc);
             exit(-1);
