d798a931a2c579225b82e7332cb04a53e5f835f4
Ypisds <111095902+Ypisds@users.noreply.github.com>
2023-08-29 11:40:36 -0300

    codigo limpo e comentado :)

--- Projetos/Projeto2/q6.c
+++ Projetos/Projeto2/q6.c
@@ -12,11 +12,11 @@ typedef struct {
 } pair;
 
 
-int tamanho;
+int tamanho; // tamanho estático
 
 
 
-void f(int i) {
+void f(int i) { // função f(pode ser qualquer uma)
     printf("numero: %d\n", i);
 }
 
@@ -39,7 +39,7 @@ typedef struct {
     void (*f)(int);
     int isTaken;
     int done;
-} paramDinamico;
+} paramDinamico; // tipo do recurso compartilhado
 
 typedef struct {
     int inicio;
@@ -47,7 +47,7 @@ typedef struct {
     int passo;
     int chunk_size;
     int N;
-} paramPai;
+} paramPai; // tipo do parametro para o processo pai
 
 paramDinamico *param;
 pthread_mutex_t lock;
@@ -56,7 +56,7 @@ pthread_cond_t taken;
 
 
 void *pai_dinamico(void *pai) {
-    pthread_mutex_lock(&lock);
+    pthread_mutex_lock(&lock); // inicialização(buscando valores para que o loop sempre dê certo)
     paramPai *Pai = (paramPai *) pai;
 
     int restante = floor(((Pai -> fim) - (Pai -> inicio)) / (Pai ->passo));
@@ -68,19 +68,20 @@ void *pai_dinamico(void *pai) {
     param -> fim = -1;
     pthread_mutex_unlock(&lock);
     
-    while(restante >= (Pai -> chunk_size)) {
-        pthread_mutex_lock(&lock);
-        while(param -> isTaken == 0) {
-            pthread_cond_wait(&taken, &lock);
+    while(restante >= (Pai -> chunk_size)) { // se o nº de iterações for maior que o chunk_size, pode-se atualizar o recurso com-
+    // partilhado com iterações de tamanho chunk_size
+        pthread_mutex_lock(&lock); // lock no mutex(para alterar)
+        while(param -> isTaken == 0) { // se a última iteração não for escolhida por nenhuma thread filha, wait.
+            pthread_cond_wait(&taken, &lock); 
         }
-        if(param -> fim == -1) {
+        if(param -> fim == -1) { // inicialização(indentificado com fim = -1)
             param -> fim = (param -> inicio) + (Pai -> chunk_size - 1) * (param -> passo);
             param -> isTaken = 0;
             restante -= (Pai -> chunk_size);
-            pthread_cond_broadcast(&notTaken);
+            pthread_cond_broadcast(&notTaken); // desbloqueia todos aqueles que estavam esperando iterações
             pthread_mutex_unlock(&lock);
         }
-        else {
+        else { // define os valores de inicio e fim com base nos dados que estavam anteriormente no recurso compartilhado
             param -> inicio = (param -> inicio) + (Pai -> chunk_size) * (param -> passo);
             param -> fim = param -> inicio + (Pai -> chunk_size - 1) * (param -> passo);
             param -> isTaken = 0;
@@ -92,7 +93,8 @@ void *pai_dinamico(void *pai) {
     }
 
     
-        pthread_mutex_lock(&lock);
+        pthread_mutex_lock(&lock); // esse caso trabalha quanto restante(iterações) < num_chunks, distribuindo um valor menor de
+        // iterações
         while(param -> isTaken == 0) {
             pthread_cond_wait(&taken, &lock);
         }
@@ -117,9 +119,9 @@ void *pai_dinamico(void *pai) {
 void *filho_dinamico() {
     int start, passo, final, done;
     while(1) {
-        final = start - 1;
+        final = start - 1; // feito para que, por segurança, não execute o for indevidamente antes de iniciar as variaveis
         pthread_mutex_lock(&lock);
-        if(param -> isTaken == 1) {
+        if(param -> isTaken == 1) { // dorme se não houver iteração nova disponível
             pthread_cond_wait(&notTaken, &lock);
         }
         
@@ -128,17 +130,17 @@ void *filho_dinamico() {
         final = param -> fim;
         done = param -> done;
         param -> isTaken = 1;
-        pthread_cond_broadcast(&taken);
+        pthread_cond_broadcast(&taken); // acorda o processo pai ao pegar a iteração
         pthread_mutex_unlock(&lock);
 
         if(start <= final)
-            printf("Thread filho recebe iterações %d-%d\n", start, final);
+            printf("Thread filho recebe iterações %d-%d\n", start, final); // "debug"
 
         for(int i = start; i <= final; i += passo) {
             param -> f(i);
         }
 
-        if(done == 1) {
+        if(done == 1) { // se não houver iterações disponiveis, encerra a thread
             
             pthread_exit(NULL);
         } 
@@ -151,33 +153,33 @@ void *filho_dinamico() {
 
 }
 
-int resto;
+int resto; // representa o ceil(iterações / n_threads)
 
 void *pai_guided(void *pai) {
     paramPai *Pai = (paramPai *) pai;
 
     int restante = floor(((Pai -> fim) - (Pai -> inicio)) / (Pai -> passo));
-    double rest_cmp =  (Pai -> fim - Pai -> inicio) / (double) Pai -> passo;
-    if(rest_cmp > restante) restante++;
+    double rest_cmp =  (Pai -> fim - Pai -> inicio) / (double) Pai -> passo; // mais uma vez, equivalente ao ceil já que não está
+    if(rest_cmp > restante) restante++;                                      // funcionando nas nossas maquinas
 
     resto = floor(restante / (Pai -> N));
     double resto_cmp = restante / (double) (Pai -> N);
     if(resto_cmp > resto) resto++;
 
-    pthread_mutex_lock(&lock);
+    pthread_mutex_lock(&lock); // inicialização
     param -> fim = -1;
     param -> inicio = Pai -> inicio;
     param -> isTaken = 1;
     param -> passo = Pai -> passo;
     pthread_mutex_unlock(&lock);
 
-    while(restante >= Pai -> chunk_size) {
+    while(restante >= Pai -> chunk_size) { // casos em que iterações = ceil(restantes/ nº threads) ou chunk_size
         pthread_mutex_lock(&lock);
-        while(param -> isTaken == 0) {
+        while(param -> isTaken == 0) { // trava se nenhuma thread filha pegar a iteração
             pthread_cond_wait(&taken, &lock);
         }
-        int n_chunk = (resto >= (Pai -> chunk_size) ? resto : Pai -> chunk_size);
-        if(param -> fim == -1) {
+        int n_chunk = (resto >= (Pai -> chunk_size) ? resto : Pai -> chunk_size); // escolhe o maior: ceil ou chunk_size.
+        if(param -> fim == -1) { // inicialização pro loop dar certo
             param -> inicio = Pai -> inicio;
             param -> fim = param -> inicio + (n_chunk - 1) * (param -> passo);
             param -> isTaken = 0;
@@ -189,10 +191,10 @@ void *pai_guided(void *pai) {
 
             n_chunk = (resto > (Pai -> chunk_size) ? resto : Pai -> chunk_size);
             
-            pthread_cond_broadcast(&notTaken);
+            pthread_cond_broadcast(&notTaken); // acordando as threads filhas
             pthread_mutex_unlock(&lock);
         }
-        else {
+        else { // caso geral
             param -> inicio = param -> fim + param -> passo;
             param -> fim = param -> inicio + (n_chunk - 1) * (param -> passo);
             param -> isTaken = 0;
@@ -212,7 +214,8 @@ void *pai_guided(void *pai) {
 
 
     }
-    pthread_mutex_lock(&lock);
+    pthread_mutex_lock(&lock); // esse caso ocorre quando, inevitavelmente,  0 < iterações restantes < chunk_size
+    // caso restante = 0, não faz diferença
     while(param -> isTaken == 0) {
         pthread_cond_wait(&taken, &lock);
     }
@@ -244,14 +247,15 @@ void omp_for(int inicio, int passo, int final, int schedule, int chunk_size, voi
     if(schedule == 0) { // case: static
 
         int restante = floor((final - inicio) / passo);
-        double rest_cmp =  (final - inicio) / (double) passo;
-        if(rest_cmp > restante) restante++;
+        double rest_cmp =  (final - inicio) / (double) passo; // essas 3 linhas representam um ceil(final - inicio/ passo).
+        if(rest_cmp > restante) restante++; //                   porque não estava funcionando aqui.
 
-        pair **p = (pair **) malloc(N*sizeof(pair *));
+        pair **p = (pair **) malloc(N*sizeof(pair *)); // será uma vetor de vetores, cada um para uma thread.
 
         tamanho = 0;
 
-        while(restante >= N * chunk_size) {
+        while(restante >= N * chunk_size) { // isso representa a parte em que é possivel passar o mesmo chunk_size para todas as
+        // threads, ou seja, restante(numero de iterações) >= N * chunk_size. Aloca em round_robin nos vetores.
             tamanho++;
             for(int i = 0; i < N; i++) {
                 p[i] = (pair *) realloc(p[i], sizeof(pair) * tamanho);
@@ -263,8 +267,9 @@ void omp_for(int inicio, int passo, int final, int schedule, int chunk_size, voi
             restante -= N*chunk_size;
         }
         
-        if(restante != 0) {
-            if(restante % N == 0) {
+        if(restante != 0) { // Caso o numero de iterações seja menor que N * chunk_size, não será possivel distribuir igualmente
+        // o memso numero de iterações = chunk_size para todas as threads.
+            if(restante % N == 0) { // Caso mais simples: distribuir restante / N para todas as threads, se N for dividor de restante
                 int c_new = restante / N;
                 tamanho++;
                 for(int i = 0; i < N; i++) {
@@ -285,7 +290,9 @@ void omp_for(int inicio, int passo, int final, int schedule, int chunk_size, voi
                     }
                 }
             }
-            else {
+            else { // Caso haja um resto q( 0<q<N), uma unidade será distribuída para q-1 threads.
+            // exemplo restante = 10. 10 % 4 = 2. Assim, as duas primeiras threads terão uma iteração a mais.
+            // seguindo o exemplo: T0(3 it), T1(3 it), T2(2 it), T3(2 it).
                 int alterados = restante % N;
                 int c_new = floor(restante / N);
                 tamanho++;
@@ -315,7 +322,7 @@ void omp_for(int inicio, int passo, int final, int schedule, int chunk_size, voi
                     }
 
                     else {
-                        if(i == 0) {
+                        if(i == 0) { // o inc considera o deslocamento do final de cada iteração por conta do resto.
                             p[i] = (pair *) realloc(p[i], sizeof(pair) * tamanho);
                             p[i][tamanho-1].inicio = (inicio + i*chunk_size *passo) + (tamanho-1) * N *chunk_size * passo;
                             p[i][tamanho-1].fim = p[i][tamanho-1].inicio + ((c_new-1) * passo);
@@ -345,14 +352,14 @@ void omp_for(int inicio, int passo, int final, int schedule, int chunk_size, voi
         }
     }
     else if(schedule == 1) { // DYNAMIC:
-        param = (paramDinamico *) malloc(sizeof(paramDinamico));
-        param -> isTaken = 1;
+        param = (paramDinamico *) malloc(sizeof(paramDinamico)); // variavel global(recurso compartilhado)
+        param -> isTaken = 1; // impede que, antes de inicializar os valores, as threads filhas peguem valores errados
         param -> f = f;
-        pthread_mutex_init(&lock, NULL);
+        pthread_mutex_init(&lock, NULL); // inicializando mutex, condições...
         pthread_cond_init(&taken, NULL);
         pthread_cond_init(&notTaken, NULL);
 
-        paramPai *papai = (paramPai *) malloc(sizeof(paramPai));
+        paramPai *papai = (paramPai *) malloc(sizeof(paramPai)); // inicializando parametro do processo pai
         papai ->chunk_size = chunk_size;
         papai -> inicio = inicio;
         papai -> fim = final;
@@ -370,7 +377,7 @@ void omp_for(int inicio, int passo, int final, int schedule, int chunk_size, voi
         }
     }
     else if(schedule == 2) { // guided:
-        param = (paramDinamico *) malloc(sizeof(paramDinamico));
+        param = (paramDinamico *) malloc(sizeof(paramDinamico)); // recurso compartilhado e inicialização análoga ao caso 1
         param -> isTaken = 1;
         param -> f = f;
         pthread_mutex_init(&lock, NULL);
@@ -403,7 +410,9 @@ void omp_for(int inicio, int passo, int final, int schedule, int chunk_size, voi
 
 int main() {
     
-    omp_for(0, 2, 20, 2, 3, f);
+    omp_for(0, 2, 20, 0, 3, f); //  passando os parâmetros para o omp for
+
+    pthread_exit(NULL);
     
 
 }
